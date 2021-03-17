a2aefb0a00506bc3ccbf996ed3fac96a56d5ea1c
Dawid Kawiak <d.kawiak@selleo.com>
2021-03-17 08:30:46 +0100

    Add start_url, cleanup

--- src/components/Geolocation/index.js
+++ src/components/Geolocation/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
-
 const Geolocation = () => {
   const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY
   const mapStyles = {
